2d89d17771b6e2f26d034fe10f8830f48dd37e06
Eglius Navikas <navikas.eglius@gmail.com>
2019-03-13 22:26:15 +0200

    Set up as Spring Boot application

---
+++ GildedRose/src/main/java/com/gildedrose/Application.java
@@ -0,0 +1,9 @@
+package com.gildedrose;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {}
+}
