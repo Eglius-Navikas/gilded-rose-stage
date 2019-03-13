e0b67f22e13eca0c556bc90eb334e65aae31f9a8
Eglius Navikas <navikas.eglius@gmail.com>
2019-03-13 21:39:07 +0200

    Added Sulfuras, Hand of Ragnaros test

---
+++ GildedRose/src/test/java/com/gildedrose/SulfurasTest.java
@@ -0,0 +1,20 @@
+package com.gildedrose;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SulfurasTest {
+
+    @Test
+    public void sulfurasQualityNeverAlters() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 10, 80),
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80)
+        };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(80, app.items[0].quality);
+        assertEquals(80, app.items[1].quality);
+    }
+
+}
