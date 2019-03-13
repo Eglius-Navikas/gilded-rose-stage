091ad8d1aa04644f14f2bda00ca3e6ee41184f06
Eglius Navikas <navikas.eglius@gmail.com>
2019-03-13 21:28:24 +0200

    Added general item tests

---
+++ GildedRose/src/test/java/com/gildedrose/GeneralItemTest.java
@@ -0,0 +1,32 @@
+package com.gildedrose;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GeneralItemTest {
+
+    @Test
+    public void generalItemLosesQualityDaily() {
+        Item[] items = new Item[] { new Item("foo", 10, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(9, app.items[0].quality);
+    }
+
+    @Test
+    public void generalItemPastExpirationLosesQualityTwiceAsFast() {
+        Item[] items = new Item[] { new Item("foo", 0, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(8, app.items[0].quality);
+    }
+
+    @Test
+    public void qualityOfAnItemDoesNotDegradeIntoNegative() {
+        Item[] items = new Item[] { new Item("foo", 10, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+    }
+}
